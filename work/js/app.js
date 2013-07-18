0b0631a7a7363df64c7e36ca2ed1e006e206ead6
David Tucker <david@davidtucker.net>
2013-07-18 06:14:36 -0400

    Adding FastClick integration to eliminate 300ms delay on mobile devices.

--- work/js/app.js
+++ work/js/app.js
@@ -4,3 +4,8 @@ var fonts = require('./fonts');
 var responsiveNav = require('./responsiveNav');
 var social = require('./social');
 var youtube = require('./youtube');
+var fastclick = require('./vendor/fastclick/fastclick');
+
+$(function() {
+	fastclick(document.body);
+});
